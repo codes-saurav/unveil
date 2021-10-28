9fcb9eb235589beb91fe2389962f22ca4741a030
Riya verma <70049435+riya-verma-7@users.noreply.github.com>
2021-10-29 02:10:50 +0530

    Update tracker.js

--- tracker.js
+++ tracker.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var ovulationdayDate = document.querySelector('#ovulationDay')
 var nextPeriodDate = document.querySelector('#nextPeriod')
 var fertilityDay = document.querySelector('#fertileDay')
@@ -25,32 +24,4 @@ evaluate.addEventListener('click', function myfunc(e) {
 
 
 
-=======
-var ovulationdayDate = document.querySelector('#ovulationDay')
-var nextPeriodDate = document.querySelector('#nextPeriod')
-var fertilityDay = document.querySelector('#fertileDay')
-
-var evaluate = document.querySelector('.myday3')
-
-// function myFunc() {
-
-evaluate.addEventListener('click', function myfunc(e) {
-    e.preventDefault();
-    let periodDate = document.querySelector(".myday").value;
-    let cycleLength = document.querySelector('.myday2').value;
-
-    let daysInMonth = 31;
-    let dayleft = daysInMonth - periodDate;
-    let nextPeriod = dayleft + Number(cycleLength) + 1;
-    let ovulateday = cycleLength - 14;
-    let fertilityWindow = (Number(ovulateday) - 2) + "," + (Number(ovulateday) - 1) + "," + Number(ovulateday);
-
-
-    ovulationdayDate.innerHTML = ovulateday + " " + "November";
-    nextPeriodDate.innerHTML = nextPeriod + " " + "November";
-    fertilityDay.innerHTML = fertilityWindow + " " + " November";
-
-
-
->>>>>>> 220effb9ac040cd3b894bb64ac642bb701aa47f4
 })
